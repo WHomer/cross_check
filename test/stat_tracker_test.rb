7a0eaca281218d47b7dd0446fd045ccbbe0925c4
William Homer <billyhomeriii@gmail.com>
2019-04-02 13:02:45 -0600

    Change require to require_relative

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -5,7 +5,7 @@ require './lib/stat_tracker'
 class StatTrackerTest < Minitest::Test
 
   def setup
-    @stat_tracker = StatTracker.new
+    @stat_tracker = StatTracker.new(1,2,3,4)
   end
 
   def test_it_exists
