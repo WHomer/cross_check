09578224e7f7348794ec5c827862f6522d255eaf
Brian Plantico <bplantico@gmail.com>
2019-04-02 18:46:43 -0600

    Adds percentage_visitor_wins test and method that works

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,7 @@ module Game
     result = @games.max_by{|row| row["away_goals"].to_i + row["home_goals"].to_i}
     result["away_goals"].to_i + result["home_goals"].to_i
   end
-  
+
   def lowest_total_score
     result = @games.min_by{|row| row["away_goals"].to_i + row["home_goals"].to_i}
     result["away_goals"].to_i + result["home_goals"].to_i
@@ -18,5 +18,15 @@ module Game
     end
     (game["away_goals"].to_i - game["home_goals"].to_i).abs
   end
-  
+
+  def percentage_visitor_wins
+    away_team_wins = []
+    @games.find_all do |game|
+      if game["away_goals"].to_i > game["home_goals"].to_i
+        away_team_wins << game
+      end
+    end
+    (away_team_wins.count.to_f / @games.count).round(2)
+  end
+
 end
