93eaa6e348ce1e5d3e8778cd00a436d410fed51f
Brian Plantico <bplantico@gmail.com>
2019-04-02 18:01:49 -0600

    Adds biggest_blowout method

--- lib/game.rb
+++ lib/game.rb
@@ -6,4 +6,12 @@ module Game
     result["away_goals"].to_i + result["home_goals"].to_i
   end
 
+  def biggest_blowout
+    # Highest difference between winner and loser
+    game = @games.max_by do |row|
+      (row["away_goals"].to_i - row["home_goals"].to_i).abs
+    end
+    (game["away_goals"].to_i - game["home_goals"].to_i).abs
+  end
+
 end
