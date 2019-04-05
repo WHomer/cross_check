f434f170bcdc2e736bf590a58c3f60735cafa414
Martha Troubh <indexer@icloud.com>
2019-04-05 07:39:03 -0600

    lo/hi goal tests and hi/lo win percentages

--- test/team_test.rb
+++ test/team_test.rb
@@ -34,16 +34,26 @@ class TeamTest < Minitest::Test
   end
 
   def test_average_win_percentage_of_all_games_for_a_team
-    assert_equal 0.52, @@stat_tracker.worst_season('18')
+    assert_equal 0.52, @@stat_tracker.average_win_percentage('18')
   end
 
+  def test_team_with_highest_number_of_goals_in_a_game
+    assert_equal 9, @@stat_tracker.most_goals_scored('18')
+  end
+
+  def test_team_with_lowest_number_of_goals_in_a_game
+    assert_equal 0, @@stat_tracker.fewest_goals_scored('18')
+  end
+
+  def test_name_of_team_with_lowest_win_percentage_against_given_team
+    assert_equal 'Oilers', @@stat_tracker.favorite_opponent('18')
+  end
+
+  def test_name_of_team_with_higest_win_percentage_against_given_team
+    assert_equal 'Red Wings', @@stat_tracker.rival('18')
   end
 
 
-
-  end
-
-  end
 
 
 end
