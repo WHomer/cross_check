f1ef670f3dd452e7b33f019eea44dd80be6ab8e8
Martha Troubh <indexer@icloud.com>
2019-04-05 17:15:02 -0600

    added test files to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,7 @@ require 'minitest/autorun'
 require 'minitest/emoji'
 require './lib/stat_tracker'
 require './test/game_test.rb'
+require './test/league_test.rb'
+require './test/season_test.rb'
+require './test/team_test.rb'
 require "pry"
